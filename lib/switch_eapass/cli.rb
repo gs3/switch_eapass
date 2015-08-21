7fb441ee6a6fda6f44c490d5fbf3e45c65a6a0e8
gs3 <doub1e.31415@gmail.com>
2015-08-21 21:35:08 +0900

    option is unnnecessary

--- lib/switch_eapass/cli.rb
+++ lib/switch_eapass/cli.rb
@@ -5,7 +5,6 @@ require 'thor'
 module SwitchEapass
   class CLI < Thor
     desc "switch <e-Pass ID>", "switch referring e-Amusement pass"
-    options :i
     def switch(card_number)
       ea = EAmusement.new
       ea.switch_reference(card_number)
