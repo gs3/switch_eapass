48fd425555402d2c2e785c4f8b766356a154d30e
gs3 <doub1e.31415@gmail.com>
2015-08-22 15:56:19 +0900

    unnecessarize to specify actual command in gem

--- lib/switch_eapass/cli.rb
+++ lib/switch_eapass/cli.rb
@@ -4,6 +4,8 @@ require 'thor'
 
 module SwitchEapass
   class CLI < Thor
+    default_command :switch
+
     desc "switch <e-Pass ID>", "switch referring e-Amusement pass"
     def switch(card_number)
       ea = EAmusement.new
